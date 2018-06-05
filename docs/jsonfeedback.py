00aa9a5291b26392dc8090207c3c341fdb5ff999
Shai25 <shaileevora25@gmail.com>
2018-06-04 20:52:29 -0400

    JSON Feedback Python Code

--- docs/jsonfeedback.py
+++ docs/jsonfeedback.py
@@ -46,7 +46,6 @@ data['details']['attack_results'].append({
     'confidence': '89%'
 })
 
-
 data['graph_link'] = 'https://www.smartdraw.com/bar-graph/'
 data['suggestion'] = 'Your model can be made more robust by training it with some of the adversarial examples which you can download for free from your dashboard.'  
 
