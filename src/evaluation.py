e20c6924ff8f5725630fb57a065b5373ad3e2607
RyanTang1 <htang1@andrew.cmu.edu>
2018-06-18 21:00:21 -0400

    update new version of evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,24 +7,14 @@ Created on Sat Jun  2 12:09:17 2018
 """
 import numpy as np
 import time
-import tensorflow as tf
-import keras
 import json
-import pickle
-import os
-import scipy.io as sio
 import argparse
-from keras.models import load_model
-from keras.layers import Input
-from keras import backend
-from keras import utils
-from keras.preprocessing.image import load_img,img_to_array
-from keras.applications.vgg16 import VGG16,preprocess_input
+from keras.applications.vgg16 import VGG16
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model",help="model path")
-parser.add_argument("--index",help="index path")
+parser.add_argument("--model",help="model path",default='.')
+parser.add_argument("--index",help="index path",default='./imagenet_class_index.json')
 
 
 
@@ -38,7 +28,7 @@ The class takes model's path and its json file's path as input
 class Model_Evaluator(object):
     def __init__(self,model_path,json_path):
         super(Model_Evaluator,self).__init__()
-        self.set_path=['CLEAN','FGSM','Mi-FGSM','I-FGSM','MADRY']
+        self.set_path=['CLEAN','FGSM','Mi-FGSM','I-FGSM']
         
         
         ##These methods to change when integrated
@@ -57,8 +47,8 @@ class Model_Evaluator(object):
     #Private functions that only be called by init
     def load_set(self,set_):
         my_dict={}
-        inputs=np.load(open('./evaluate_input_'+set_+'.npy'))
-        labels=np.load(open('./evaluate_label_'+set_+'.npy'))
+        inputs=np.load(open('./image_data/evaluate_input_'+set_+'.npy'))
+        labels=np.load(open('./image_data/evaluate_label_'+set_+'.npy'))
         for key,input_ in zip(labels,inputs):
             if key not in my_dict:
                 my_dict[key]=[]
@@ -70,11 +60,12 @@ class Model_Evaluator(object):
     #Private functions that only will be called by evaluate
     def decode_predictions(self,predict):
         argmax=np.argmax(predict,axis=1)
+        confidence=np.max(predict,axis=1).sum()
         
         decode=[]
         for label in argmax:
             decode.append(self.class_index[str(label)][0]) #need change after deployment
-        return np.array(decode)
+        return np.array(decode),confidence
         
     def evaluate(self):
         '''
@@ -88,19 +79,20 @@ class Model_Evaluator(object):
                 y=key
     
                 batch=np.vstack(data[y])
-                predict=self.decode_predictions(self.model.predict(batch,batch_size=5))
+                predict,confidence=self.decode_predictions(self.model.predict(batch,batch_size=10))
                 right+=len(predict[predict==y])
                 total+=len(data[y])
-            return float(right)/total
+            return float(right)/total,confidence/total
             #print clean_predict.shape
         
         
         for path in self.set_path:
             start=time.time()
             inputs=self.load_set(path)
-            acc=calculate_acc(inputs)
+            acc,confidence=calculate_acc(inputs)
             end=time.time()
             print(path+" ACC: ",acc)
+            print(path+" Average Confidence: ",confidence)
             print(path+" TIME: ",end-start)
 #        
 #        adv=calculate_acc(self.adv_set)
@@ -108,9 +100,11 @@ class Model_Evaluator(object):
         
             
                 
-
-
+###Your work
+##specify what is the input here
 model=Model_Evaluator(args.model,args.index)
 model.evaluate()
+
+##What is the output of the evaluate
         
     
